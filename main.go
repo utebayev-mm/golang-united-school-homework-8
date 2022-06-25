4011e3caa7a390fc230919d4b23eee361a58ce5b
utebayev.mm <utebayev.mm@gmail.com>
2022-06-25 20:50:08 +0600

    parseArgs & structs

--- main.go
+++ main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"os"
 )
 
+type Arguments map[string]string
+
+type User struct {
+	ID    string `json:"id"`
+	Email string `json:"email"`
+	Age   uint   `json:"age"`
+}
+
 func Perform(args Arguments, writer io.Writer) error {
+	return nil
 }
 
 func main() {
 	err := Perform(parseArgs(), os.Stdout)
 	if err != nil {
 		panic(err)
+	}
+}
+
+func parseArgs() Arguments {
+	idFlag := flag.String("id", "", "")
+	itemFlag := flag.String("item", "", "")
+	operationFlag := flag.String("operation", "", "")
+	filenameFlag := flag.String("filename", "", "")
+	flag.Parse()
+
+	return Arguments{
+		"id":        *idFlag,
+		"item":      *itemFlag,
+		"operation": *operationFlag,
+		"filename":  *filenameFlag,
 	}
 }
